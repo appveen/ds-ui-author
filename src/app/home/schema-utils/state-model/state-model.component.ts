8f0fea307ae1e5cecbfab87376911fb034035571
Vignesh Saikrishna Mallya <vignesh@appveen.com>
2021-08-30 16:06:18 +0530

    remove readonly for state model if exists for existing attribute

--- src/app/home/schema-utils/state-model/state-model.component.ts
+++ src/app/home/schema-utils/state-model/state-model.component.ts
@@ -149,10 +149,14 @@ export class StateModelComponent implements OnInit {
       if (self.stateModelAttrIndex > -1) {
         self.form.get(['stateModel', 'attribute']).patchValue(this.stateModelAttribute);
         self.form.get(['definition', self.stateModelAttrIndex, 'properties', 'default']).patchValue(null);
-        self.form.get(['definition', self.stateModelAttrIndex, 'properties', 'readonly']).patchValue(false);
+
+        // remove readonly for state model if enabled for existing attribute
+        const isReadonly = self.form.get(['definition', self.stateModelAttrIndex, 'properties', 'readonly']).value;
+        if (isReadonly) {
+          self.form.get(['definition', self.stateModelAttrIndex, 'properties', 'readonly']).patchValue(false);
+        }
 
         self.form.get(['stateModel', 'enabled']).patchValue(true);
-        // const existingStates = self.form.get(['definition', self.stateModelAttrIndex, 'properties', 'enum']).value;
         if (self.allStates.length > 0) {
           const stateModelDict = self.allStates.reduce((a, x) => ({ ...a, [x]: [] }), {});
           self.form.get(['stateModel', 'states']).patchValue(stateModelDict);
