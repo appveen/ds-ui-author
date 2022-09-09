22411b26025a439ba521ff638a3768353e00b617
Jugnu Agrawal <jugnu@appveen.com>
2022-09-09 16:50:50 +0530

    cleanup

--- src/app/home/b2b-flows/b2b-flows.module.ts
+++ src/app/home/b2b-flows/b2b-flows.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AgGridModule } from 'ag-grid-angular';
 
 import { B2bFlowsComponent } from './b2b-flows.component';
 import { ClickOutsideModule } from 'src/app/utils/directives/click-outside/click-outside.module';
@@ -11,8 +10,6 @@ import { BreadcrumbModule } from 'src/app/utils/breadcrumb/breadcrumb.module';
 import { SearchBoxModule } from 'src/app/utils/search-box/search-box.module';
 import { DeleteModalModule } from 'src/app/utils/delete-modal/delete-modal.module';
 import { FormatTypeBadgeModule } from 'src/app/utils/format-type-badge/format-type-badge.module';
-import { B2bFlowCellComponent } from './b2b-flow-cell/b2b-flow-cell.component';
-import { B2bFlowFilterComponent } from './b2b-flow-filter/b2b-flow-filter.component';
 import { BasicInfoModule } from 'src/app/utils/basic-info/basic-info.module';
 import { CodeEditorModule } from 'src/app/utils/code-editor/code-editor.module';
 import { DateFormatModule } from 'src/app/utils/date-format/date-format.module';
@@ -25,7 +22,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [B2bFlowsComponent, B2bFlowCellComponent, B2bFlowFilterComponent],
+  declarations: [B2bFlowsComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
@@ -40,10 +37,6 @@ const routes: Routes = [
     BasicInfoModule,
     BreadcrumbModule,
     CodeEditorModule,
-    AgGridModule.withComponents([
-      B2bFlowCellComponent,
-      B2bFlowFilterComponent
-    ]),
     DateFormatModule,
     OnChangeModule
   ],
