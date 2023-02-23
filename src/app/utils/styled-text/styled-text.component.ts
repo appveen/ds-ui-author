6a80e29bb82ff77eb2680c1bd2f30de22b4cc5cf
Utcrash <utkarsh@appveen.com>
2023-02-23 10:06:00 +0530

    Attempt for the badge input -1

--- src/app/utils/styled-text/styled-text.component.ts
+++ src/app/utils/styled-text/styled-text.component.ts
@@ -1,83 +1,47 @@
-import { Component, ElementRef, Input, OnInit, ViewChild, ViewEncapsulation, } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, ViewChild, ViewEncapsulation, } from '@angular/core';
+
 
 @Component({
   selector: 'odp-styled-text',
-  template: `
-    <div #editableDiv id="editableDiv" class="value-holder" contentEditable="true" (input)="onInput($event)" ></div>
-    <div [innerHTML]="formattedText"></div>
-  `,
-  styles: [`
-  .value-holder {
-    width: 260px;
-    border: 0px;
-    border-left: 1px solid #ddd;
-    height: 100%;
-}
-  `],
+  templateUrl: './styled-text.component.html',
+  styleUrls: ['styled-text.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class StyledTextComponent implements OnInit {
   regex: RegExp = /\${{([^}]*)}}/g;
   @Input() cssClass: string;
   @Input() cssStyle: string;
+  @Input() onDblClick: EventEmitter<any> = new EventEmitter();
   unformattedText: string = '';
   formattedText: string = '';
-
-  @ViewChild('editableDiv', { static: true }) editableDiv: ElementRef;
+  @ViewChild('contentDiv') contentDiv: ElementRef;
+  @ViewChild('hiddenDiv') hiddenDiv: ElementRef;
   constructor() { }
 
   ngOnInit() {
     this.formatText();
+
   }
 
-
-  moveCursorToEnd() {
-    var el = document.getElementById('editableDiv') as any
-    el.focus()
-    if (typeof el.selectionStart == "number") {
-      el.selectionStart = el.selectionEnd = el.value.length;
-    } else if (typeof el.createTextRange != "undefined") {
-      var range = el.createTextRange();
-      range.collapse(false);
-      range.select();
-    }
-  }
-
-  onInput(event: InputEvent) {
-    this.unformattedText = this.editableDiv.nativeElement.innerText;
+  someTrigger(event) {
+    // console.log(event.target.innerText)
+    this.unformattedText = event.target.innerText;
     this.formatText();
-    this.moveCursorToEnd();
-
   }
 
 
   formatText() {
-    if (this.editableDiv.nativeElement.innerHTML) {
-      const matches = this.editableDiv.nativeElement.innerHTML.match(this.regex) || [];
-
-      this.formattedText = this.unformattedText.replace(this.regex, `<span class="fw-500 rounded bg-light p-2 mx-1 border font-12 d-flex align-items-center">$&</span>`);
-      // this.unformattedText = this.formattedText.replace(/<\/?span[^>]*>/g, '');
-
-      this.editableDiv.nativeElement.innerHTML = this.formattedText;
+    if (this.unformattedText) {
+      const pattern = new RegExp(this.regex, 'g');
+      this.formattedText = this.unformattedText.replace(this.regex, `<span class="d-flex align-items-center text-primary">$&</span>`);
     } else {
       this.formattedText = '';
-      this.unformattedText = '';
     }
   }
-  // formatText() {
 
-  //   if (this.unformattedText) {
-  //     const pattern = new RegExp(this.regex, 'g');
-  //     const matches = this.unformattedText.match(pattern);
-  //     if (matches) {
-  //       this.formattedText = this.unformattedText.replace(pattern, `<span class="fw-500 rounded bg-light p-2 mx-1 border font-12 d-flex align-items-center">$&</span>`);
-  //       this.editableDiv.nativeElement.innerHTML = this.formattedText;
-  //     }
-
-  //   } else {
-  //     this.formattedText = '';
-  //   }
-  // }
+  check() {
+    console.log('yes')
+  }
 
 }
 
