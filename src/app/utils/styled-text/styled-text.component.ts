080fbad8cca234ae4bee9aafa86294a1047ee6cc
Utcrash <utkarsh@appveen.com>
2023-03-01 21:40:25 +0530

    Attempt to fix the cursor

--- src/app/utils/styled-text/styled-text.component.ts
+++ src/app/utils/styled-text/styled-text.component.ts
@@ -37,77 +37,72 @@ export class StyledTextComponent implements OnInit {
   }
 
   someTrigger(event, value?) {
+    const oldText = this.unformattedText;
     this.unformattedText = event?.target?.innerText || value;
+    const index = this.findFirstDifference(this.unformattedText, oldText)
+    console.log(index)
     this.suggestions = _.cloneDeep(this.list)
     this.unformattedText = this.unformattedText ? this.unformattedText.replace(/(\r\n|\n|\r)/gm, "") : '';
-    let term = this.unformattedText.split(/[\{\}]+/).pop();
-
-    console.log(term)
+    let term = this.unformattedText.split(/[\{\}\s]+/).pop();
     this.searchTerm = term && term.trim() !== '' ? [term.trim()] : null;
-    this.formatText();
-    this.setRange();
-
     if (this.searchTerm && this.searchTerm[0] !== '') {
       this.search(this.searchTerm).subscribe(results => {
         this.suggestions = results;
       });
     }
-
+    this.formatText();
+    this.setRange()
+    // this.setCaretPosition(this.editableDiv, index);
 
   }
 
 
+  findFirstDifference(str1 = '', str2 = '') {
+    for (let i = 0; i < Math.max(str1.length, str2.length); i++) {
+      if (str1[i] !== str2[i]) {
+        return i;
+      }
+    }
+    return str1.length === str2.length ? -1 : Math.min(str1.length, str2.length);
+  }
+
   formatText() {
     if (this.unformattedText) {
       const pattern = new RegExp(this.regex, 'g');
+
       this.formattedText = this.unformattedText.replace(pattern, `<span class="d-flex align-items-center text-primary">$&</span>`);
     } else {
       this.formattedText = '';
     }
 
+
+    // const finalHtml = this.formatOther(this.formattedText)
+
     this.editableDiv.innerHTML = this.formattedText;
+
   }
 
   setRange() {
-    this.getCaretPosition(this.editableDiv);
     let sel = window.getSelection();
-    console.log(document.getSelection().getRangeAt(0).startOffset)
     sel.selectAllChildren(this.editableDiv);
     sel.collapseToEnd();
   }
 
-
-  // getCaretPosition(editableDiv) {
-  //   var caretPosition = 0;
-  //   var range = window.getSelection().getRangeAt(0);
-  //   var preCaretRange = range.cloneRange();
-  //   preCaretRange.selectNodeContents(editableDiv);
-  //   preCaretRange.setEnd(range.endContainer, range.endOffset);
-  //   caretPosition = preCaretRange.toString().length;
-  //   return caretPosition;
-  // }
-
-
-  getCaretPosition(editableDiv) {
-    var caretPos = 0,
-      sel, range;
-    if (window.getSelection) {
-      sel = window.getSelection();
-      if (sel.rangeCount) {
-        range = sel.getRangeAt(0);
-        if (range.commonAncestorContainer.parentNode == editableDiv) {
-          caretPos = range.endOffset;
-        }
-      }
-    }
-    return caretPos;
+  setCaretPosition(el, index) {
+    var range = document.createRange();
+    var sel = window.getSelection();
+    range.setStart(el.childNodes[0], index + 1);
+    range.collapse(true);
+    sel.removeAllRanges();
+    sel.addRange(range);
+    el.focus();
   }
+
 
   selectItem(suggestion) {
 
     const val = '${{' + suggestion + '}}';
-    const completeText = this.editableDiv.innerText.replace(new RegExp(this.searchTerm.toString() + '$'), ' ' + val + ' ');
-    // const completeText = this.editableDiv.innerText + ' ' + val;
+    const completeText = this.editableDiv.innerText.replace(new RegExp(this.searchTerm.toString() + '$'), val);
     this.searchTerm = []
     this.someTrigger(null, completeText);
 
@@ -121,5 +116,33 @@ export class StyledTextComponent implements OnInit {
     });
     return of(matches);
   }
+
+
+  formatOther(str) {
+    const pattern = new RegExp('<span (.*?)<\/span>', 'g');
+    const match = str.match(pattern);
+    let replacement = '';
+    for (let i = 0; i < str.length; i++) {
+      let char = str.charAt(i);
+      let found = false;
+      if (match) {
+
+        for (let j = 0; j < match.length; j++) {
+          if (i >= str.indexOf(match[j]) && i < str.indexOf(match[j]) + match[j].length) {
+            replacement += char;
+            found = true;
+            break;
+          }
+        }
+      }
+
+      if (!found) {
+        replacement += "<p>" + char + "</p>";
+      }
+    }
+
+    return replacement
+  }
+
 }
 
